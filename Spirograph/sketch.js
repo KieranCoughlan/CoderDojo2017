3e1918904f62746a420abaabcf990ed0e4b1cbc8
Kieran Coughlan <Kieran.Coughlan@woodgroupkenny.com>
2018-01-24 10:32:30 +0000

    Converted to using vectors.

--- Spirograph/sketch.js
+++ Spirograph/sketch.js
@@ -1,16 +1,13 @@
-let spirograph0;
+let spirograph;
 
 function setup() {
     createCanvas(800, 600);
     background('black');
 
-    spirograph0 = new Spirograph(410, 300, 180, 43, 0.5, 3.58, 'red');
-    spirograph1 = new Spirograph(390, 300, 150, 33, 0.5, 7.1, 'yellow');
-
+    spirograph = new Spirograph(createVector(400, 300), 200, 43, 0.5, 3.58, 'red');
 }
 
 function draw() {
-    spirograph0.show();
-    spirograph1.show();
+    spirograph.show();
 }
 
--- Spirograph/spirograph.js
+++ Spirograph/spirograph.js
@@ -1,55 +1,43 @@
-class Spirograph{
-    constructor(centerX, centerY, rad0, rad1, speed0, speed1, colour){
-        this.centerX = centerX;
-        this.centerY = centerY;
-
-        this.rad0 = rad0;
-        this.rad1 = rad1;
-
+class Spirograph {
+    constructor(center, radius0, radius1, speed0, speed1, colour) {
+        this.center = center;
+        this.radius0 = radius0;
+        this.radius1 = radius1;
         this.speed0 = speed0;
         this.speed1 = speed1;
-
         this.colour = colour;
-        
-        this.firstPoint = true;
-        this.x = 0;
-        this.y = 0;
-        this.lastX = 0;
-        this.lastY = 0;
 
+        this.firstPoint = true;
+        this.point = createVector(0, 0);
+        this.lastPoint = createVector(0, 0);
         this.angle0 = 0;
         this.angle1 = 0;
     }
 
-    show(){
+    show() {
         this.calculatePos();
 
         stroke(this.colour);
 
-        if (this.firstPoint == true){
-            this.firstPoint = false;           
+        if (this.firstPoint == true) {
+            this.firstPoint = false;
         }
-        else{
-            line(this.lastX, this.lastY, this.x, this.y);
+        else {
+            line(this.lastPoint.x, this.lastPoint.y, this.point.x, this.point.y);
         }
 
-        this.lastX = this.x;
-        this.lastY = this.y;
+        this.lastPoint = this.point;
     }
 
-    calculatePos(){
+    calculatePos() {
         angleMode(DEGREES);
 
         this.angle0 += this.speed0;
         this.angle1 += this.speed1;
-        
-        this.x = this.centerX;
-        this.y = this.centerY;
-        
-        this.x += this.rad0 * cos(this.angle0);
-        this.y += this.rad0 * sin(this.angle0);
 
-        this.x += this.rad1 * cos(this.angle1);
-        this.y += this.rad1 * sin(this.angle1);
+        this.point = this.center.copy();
+
+        this.point.add(this.radius0 * cos(this.angle0), this.radius0 * sin(this.angle0));
+        this.point.add(this.radius1 * cos(this.angle1), this.radius1 * sin(this.angle1));
     }
 }
