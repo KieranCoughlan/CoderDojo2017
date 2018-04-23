0eb56cac398ca924b5f52700a5d9fff926b4f48b
Kieran Coughlan <Kieran.Coughlan@woodgroupkenny.com>
2018-04-23 16:57:09 +0100

    Updated comments

--- Continent/sketch.js
+++ Continent/sketch.js
@@ -5,15 +5,15 @@ function setup() {
 
     // I'm using a low frame rate and ContinentDyn here so you can see the 
     // process dynamically. You'll probably want to use Continent normally.
-    // Note: number of itterations don't need to be too high. After a certain
+    // Note: number of itterations doesn't need to be too high. After a certain
     // few you can't see any difference. Experiement with the numbers yourself.
     frameRate(1);
-    continent = new ContinentDyn(200, 16, 50);
+    continent = new ContinentDyn(200, 12, 50);
 }
 
 function draw(){
     background('black');
-    translate(innerWidth/2, innerHeight/2);
+    translate(innerWidth / 2, innerHeight / 2);
     continent.show();
 }
 
